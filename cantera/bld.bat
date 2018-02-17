ab979954b4125a62febeb9599c3e1075f6891df1
Ray Speth <speth@mit.edu>
2018-02-17 16:42:08 -0500

    Simplify build thanks to SCons 3.0 compatibility with Python 3

--- cantera/bld.bat
+++ cantera/bld.bat
@@ -6,30 +6,18 @@ IF %ARCH% EQU 64 (
 	CALL "%VS140COMNTOOLS%"\..\..\VC\bin\vcvars32.bat
 )
 
-:: Remove the old builder environment, if it exists
-CALL conda env remove -yq -p "%PREFIX%\..\cantera-builder"
-
-:: Create a conda environment to build Cantera. It has to be Python 2, for
-:: Scons compatibility. When SCons is available for Python 3, these machinations
-:: can be removed
-:: Important: As of 15-Oct-2016, the most recent version of SCons available in
-:: the conda repositories is 2.3.0. Unfortunately, using VS 2015 requires SCons
-:: 2.4.1 or higher. This is available from the cantera channel on anaconda.org,
-:: so we add `-c cantera/label/builddeps` to pick up SCons from that channel.
-:: In addition, `3to2` is only available as a conda package from the
-:: `cantera/label/builddeps` channel.
-CALL conda create -yq -p "%PREFIX%\..\cantera-builder" -c cantera/label/builddeps python=2 cython pywin32 scons 3to2 boost
-
-:: The major version of the Python that will be used for the installer, not the
-:: version used for building
+:: The major version of Python being used
 SET PY_MAJ_VER=%PY_VER:~0,1%
+
+IF %PY_MAJ_VER% EQU 2 (
+    CALL conda install -y -c cantera/label/builddeps 3to2
+)
+
+CALL conda install -y -c conda-forge scons
 
 :: Set the number of CPUs to use in building
 SET /A CPU_USE=%CPU_COUNT% / 2
 IF %CPU_USE% EQU 0 SET CPU_USE=1
-
-SET OLD_CONDA_ENV="%CONDA_DEFAULT_ENV%"
-CALL "%ROOT%\Scripts\activate.bat" "%PREFIX%\..\cantera-builder"
 
 :: Have to use CALL to prevent the script from exiting after calling SCons
 CALL scons clean
@@ -43,29 +31,8 @@ ECHO f90_interface='n' >> cantera.conf
 ECHO system_sundials='n' >> cantera.conf
 
 SET "ESC_PREFIX=%PREFIX:\=/%"
-ECHO boost_inc_dir="%ESC_PREFIX%/../cantera-builder/Library/include" >> cantera.conf
+ECHO boost_inc_dir="%ESC_PREFIX%/Library/include" >> cantera.conf
 
-:: Select which version of the interface should be built
-IF "%PY_MAJ_VER%" EQU "2" GOTO PYTHON2
-IF "%PY_MAJ_VER%" EQU "3" GOTO PYTHON3
-
-:PYTHON2
-ECHO Building for Python 2
-CALL scons build -j%CPU_USE% python3_package=n python_cmd="%PYTHON%" python_package=full
-GOTO BUILD_SUCCESS
-
-:PYTHON3
-ECHO Building for Python 3
-CALL scons build -j%CPU_USE% python3_package=y python3_cmd="%PYTHON%" python_package=none
-GOTO BUILD_SUCCESS
-
-:BUILD_SUCCESS
-:: Remove the builder environment and reset the path
-CALL "%ROOT%\Scripts\activate.bat" "%OLD_CONDA_ENV%"
-CALL conda env remove -yq -p "%PREFIX%"\..\cantera-builder
-:: SET PATH=%OLD_PATH%
-
-:: Change to the Python interface directory and run the installer using the
-:: proper version of Python.
+CALL scons build -j%CPU_USE% python_package=y python_cmd="%PYTHON%"
 cd interfaces/cython
 "%PYTHON%" setup%PY_MAJ_VER%.py build --build-lib=../../build/python%PY_MAJ_VER% install
--- cantera/build.sh
+++ cantera/build.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-# Remove the old builder environement, if it exists
-if [[ -d "$PREFIX/../cantera-builder" ]]; then
-    conda env remove -yq -p "$PREFIX/../cantera-builder"
+if [ "${PY_MAJ_VER}" == "2" ]; then
+    conda install -y -c cantera/label/builddeps 3to2
 fi
-
-# Create a conda environment to build Cantera. It has to be Python 2, for
-# SCons compatibility. When SCons is available for Python 3, these machinations
-# can be removed
-conda create -yq -p "$PREFIX/../cantera-builder" -c cantera/label/builddeps python=2 scons cython 3to2 boost
-
-# The major version of the Python that will be used for the installer, not the
-# version used for building
-PY_MAJ_VER=${PY_VER:0:1}
-
-OLD_CONDA_ENV="$CONDA_DEFAULT_ENV"
-set +x
-source activate "$PREFIX/../cantera-builder"
+conda install -y -c conda-forge scons
 
 scons clean
 
@@ -25,7 +12,7 @@ echo "matlab_toolbox='n'" >> cantera.conf
 echo "f90_interface='n'" >> cantera.conf
 echo "system_sundials='n'" >> cantera.conf
 echo "debug='n'" >> cantera.conf
-echo "boost_inc_dir = '$PREFIX/../cantera-builder/include'" >> cantera.conf
+echo "boost_inc_dir = '$PREFIX/include'" >> cantera.conf
 
 if [[ "$CONDA_ARCH" == "linux_x86" ]]; then
   echo "cc_flags='-m32'" >> cantera.conf
@@ -40,17 +27,7 @@ fi
 set -x
 
 # Run SCons to build the proper Python interface
-if [ "${PY_MAJ_VER}" == "2" ]; then
-    scons build -j$((CPU_COUNT/2)) python3_package='n' python_cmd=$PYTHON python_package='full'
-else
-    scons build -j$((CPU_COUNT/2)) python3_package='y' python3_cmd=$PYTHON python_package='none'
-fi
-
-# Remove the builder environment
-set +x
-source activate "$OLD_CONDA_ENV"
-set -x
-conda env remove -yq -p "$PREFIX/../cantera-builder"
+scons build -j$((CPU_COUNT/2)) python_package=y python_cmd=$PYTHON
 
 # Change to the Python interface directory and run the installer using the
 # proper version of Python.
