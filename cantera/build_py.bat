d433b59bda28bf827d209c7ef1eb679bbb66e391
Bryan W. Weber <bryan.w.weber@gmail.com>
2018-11-21 16:22:45 -0500

    Exit the Python build script if building the Python interface fails

--- cantera/build_py.bat
+++ cantera/build_py.bat
@@ -1,4 +1,5 @@
 SET "ESC_PYTHON=%PYTHON:\=/%"
 CALL scons build python_package=y python_cmd="%ESC_PYTHON%"
+IF ERRORLEVEL 1 EXIT 1
 cd interfaces/cython
 "%PYTHON%" setup.py build --build-lib=../../build/python install
