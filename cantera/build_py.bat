c57c6d596ea83db0102cb3121f1709cdbe6a004c
Bryan W. Weber <bryan.w.weber@gmail.com>
2018-11-21 16:22:19 -0500

    Use an escaped Python executable when building on Windows

--- cantera/build_py.bat
+++ cantera/build_py.bat
@@ -1,3 +1,4 @@
-CALL scons build python_package=y python_cmd="%PYTHON%"
+SET "ESC_PYTHON=%PYTHON:\=/%"
+CALL scons build python_package=y python_cmd="%ESC_PYTHON%"
 cd interfaces/cython
 "%PYTHON%" setup.py build --build-lib=../../build/python install
