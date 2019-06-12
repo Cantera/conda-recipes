4710292c2bc92609d90ba64594edca44158c17be
Bryan W. Weber <bryan.w.weber@gmail.com>
2019-06-12 13:03:51 -0400

    Clean up build scripts for libcantera build

--- cantera/build_py.sh
+++ cantera/build_py.sh
@@ -1,13 +1,11 @@
 echo "****************************"
 echo "PYTHON BUILD STARTED"
-echo "$PY_VER    $NPY_VER"
-echo "PYTHON IS $PYTHON"
 echo "****************************"
 
 scons build python_package='y' python_cmd="${PYTHON}"
 
 echo "****************************"
-echo "BUILD COMPLETED SUCCESSFULLY"
+echo "PYTHON BUILD COMPLETED SUCCESSFULLY"
 echo "****************************"
 
 cd interfaces/cython
