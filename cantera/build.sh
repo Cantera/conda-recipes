dc7c0bdbb71182e7770887a449512f85e79dad8b
Bryan W. Weber <bryan.w.weber@gmail.com>
2019-06-12 13:03:51 -0400

    Clean up build scripts for libcantera build

--- cantera/build.sh
+++ cantera/build.sh
@@ -2,18 +2,27 @@ set +x
 
 echo "****************************"
 echo "LIBRARY BUILD STARTED"
-echo "CONDA_BUILD_SYSROOT IS $CONDA_BUILD_SYSROOT"
 echo "****************************"
 
-scons clean
+if [[ "$DIRTY" != "1" ]]; then
+    scons clean
+fi
 
 rm -f cantera.conf
 
 # Use the compilers from the Conda environment
-echo "CC = '$CC'" >> cantera.conf
-echo "CXX = '$CXX'" >> cantera.conf
+if [[ "${OSX_ARCH}" == "" ]]; then
+    echo "CC = '${CC}'" >> cantera.conf
+    echo "CXX = '${CXX}'" >> cantera.conf
+    echo "blas_lapack_libs = 'mkl_rt,dl'" >> cantera.conf
+    echo "blas_lapack_dir = '${PREFIX}/lib'" >> cantera.conf
+else
+    echo "CC = '${CLANG}'" >> cantera.conf
+    echo "CXX = '${CLANGXX}'" >> cantera.conf
+    echo "cc_flags = '-isysroot ${CONDA_BUILD_SYSROOT} -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}'" >> cantera.conf
+fi
 
-echo "prefix = '$PREFIX'" >> cantera.conf
+echo "prefix = '${PREFIX}'" >> cantera.conf
 echo "use_pch = False" >> cantera.conf
 
 # We want neither the MATLAB interface nor the Fortran interface
@@ -21,22 +30,17 @@ echo "matlab_toolbox = 'n'" >> cantera.conf
 echo "f90_interface = 'n'" >> cantera.conf
 echo "system_sundials = 'n'" >> cantera.conf
 echo "debug = 'n'" >> cantera.conf
-echo "boost_inc_dir = '$PREFIX/include'" >> cantera.conf
+echo "boost_inc_dir = '${PREFIX}/include'" >> cantera.conf
 echo "python_package = 'none'" >> cantera.conf
 
-if [[ "$ARCH" == "32" ]]; then
+if [[ "${ARCH}" == "32" ]]; then
   echo "cc_flags='-m32'" >> cantera.conf
   echo "no_debug_linker_flags='-m32'" >> cantera.conf
 fi
 
-if [[ "$OSX_ARCH" == "" ]]; then
-    echo "blas_lapack_libs = 'mkl_rt,dl'" >> cantera.conf
-    echo "blas_lapack_dir = '$PREFIX/lib'" >> cantera.conf
-fi
-
 set -xe
 
-scons build -j2
+scons build -j${CPU_COUNT}
 
 set +xe
 
--- cantera/build_devel.sh
+++ cantera/build_devel.sh
@@ -1,14 +1,15 @@
 echo "****************************"
-echo "LIBRARY INSTALL STARTED"
+echo "DEVEL LIBRARY INSTALL STARTED"
 echo "****************************"
 
 set -e
 # Remove the python_cmd configuration because Python is not
 # a host dependency of the libcantera package
 sed -i.bak '/python_cmd/d' cantera.conf
+rm -f cantera.conf.bak*
 scons install python_package='none'
 set +e
 
 echo "****************************"
-echo "LIBRARY INSTALL COMPLETED SUCCESSFULLY"
+echo "DEVEL LIBRARY INSTALL COMPLETED SUCCESSFULLY"
 echo "****************************"
