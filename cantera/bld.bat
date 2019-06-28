7a7842963f67eb6fd3d2aaba3ca2acaa7cfb2a5c
bryanwweber <bryan.w.weber@gmail.com>
2019-06-28 11:43:27 -0400

    Update Windows build scripts
    Applies to libcantera and Python package. Fix various options specified to cantera.conf. Make sure the script exits if one of the commands errors.

--- cantera/bld.bat
+++ cantera/bld.bat
@@ -1,14 +1,16 @@
 @ECHO off
 
-echo "\n\n****************************"
-echo "BUILD STARTED"
-echo "****************************\n\n"
+echo ****************************
+echo BUILD STARTED
+echo ****************************
 
-IF %ARCH% EQU 64 (
-	CALL "%VS140COMNTOOLS%"\..\..\VC\bin\amd64\vcvars64.bat
-) ELSE (
-	CALL "%VS140COMNTOOLS%"\..\..\VC\bin\vcvars32.bat
-)
+:: IF %ARCH% EQU 64 (
+::	  CALL "%VS141COMNTOOLS%"\..\..\VC\bin\amd64\vcvars64.bat
+:: ) ELSE (
+::    CALL "%VS150COMNTOOLS%"\..\..\VC\bin\vcvars32.bat
+:: )
+
+DEL /F cantera.conf
 
 :: Set the number of CPUs to use in building
 SET /A CPU_USE=%CPU_COUNT% / 2
@@ -16,10 +18,11 @@ IF %CPU_USE% EQU 0 SET CPU_USE=1
 
 :: Have to use CALL to prevent the script from exiting after calling SCons
 CALL scons clean
+IF ERRORLEVEL 1 EXIT 1
 
 :: Put important settings into cantera.conf for the build. Use VS 2015 to
 :: compile the interface.
-ECHO msvc_version='14.0' >> cantera.conf
+ECHO msvc_version='14.1' >> cantera.conf
 ECHO matlab_toolbox='n' >> cantera.conf
 ECHO debug='n' >> cantera.conf
 ECHO f90_interface='n' >> cantera.conf
@@ -29,9 +32,11 @@ ECHO python_package='none' >> cantera.conf
 SET "ESC_PREFIX=%PREFIX:\=/%"
 ECHO prefix="%ESC_PREFIX%" >> cantera.conf
 ECHO boost_inc_dir="%ESC_PREFIX%/Library/include" >> cantera.conf
+ECHO use_pch = False >> cantera.conf
 
 CALL scons build -j%CPU_USE%
+IF ERRORLEVEL 1 EXIT 1
 
-echo "\n\n****************************"
+echo "****************************"
 echo "BUILD COMPLETED SUCCESSFULLY"
-echo "****************************\n\n"
+echo "****************************"
--- cantera/build_py.bat
+++ cantera/build_py.bat
@@ -1,5 +1,17 @@
+echo ****************************
+echo PYTHON %PYTHON% BUILD STARTED
+echo ****************************
+
 SET "ESC_PYTHON=%PYTHON:\=/%"
-CALL scons build python_package=y python_cmd="%ESC_PYTHON%"
+ECHO python_package='full' >> cantera.conf
+ECHO python_cmd="%ESC_PYTHON%" >> cantera.conf
+CALL scons build
 IF ERRORLEVEL 1 EXIT 1
+
+echo ****************************
+echo PYTHON %PYTHON% BUILD COMPLETED SUCCESSFULLY
+echo ****************************
+
 cd interfaces/cython
 "%PYTHON%" setup.py build --build-lib=../../build/python install
+IF ERRORLEVEL 1 EXIT 1
