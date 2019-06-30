7bb433b5b667be74bb851329a0e306c150995f6d
Bryan W. Weber <bryan.w.weber@gmail.com>
2019-06-29 21:53:07 -0400

    Use a staging directory for the Matlab package
    Copy files from the staging directory into the appropriate folders in
    the build prefix. Do this rather than using scons to install to avoid
    including libcantera stuff

--- cantera-matlab/build.sh
+++ cantera-matlab/build.sh
@@ -12,8 +12,12 @@ rm -f cantera.conf
 
 cp "${RECIPE_DIR}/../.ci_support/cantera_base.conf" cantera.conf
 
-echo "prefix = '${PREFIX}'" >> cantera.conf
+echo "prefix = ''" >> cantera.conf
 echo "boost_inc_dir = '${PREFIX}/include'" >> cantera.conf
+
+# Stage the files to make copying easier later
+STAGE_DIR="stage"
+echo "stage_dir = '${STAGE_DIR}'" >> cantera.conf
 
 if [[ "${OSX_ARCH}" == "" ]]; then
     echo "CC = '${CC}'" >> cantera.conf
@@ -32,4 +36,25 @@ set -xe
 scons build -j${CPU_COUNT}
 scons install
 
+# "Install" just the Matlab interface. This method should
+# prevent this package from clobbering any existing
+# libcantera or Cantera Python interface files, except the
+# data files and the license file.
+CT_SAMPLES_DIR="share/cantera/samples/matlab"
+mkdir -p "${PREFIX}/${CT_SAMPLES_DIR}"
+cp -R "${STAGE_DIR}/${CT_SAMPLES_DIR}/" "${PREFIX}/${CT_SAMPLES_DIR}"
+CT_DATA_DIR="share/cantera/data"
+mkdir -p "${PREFIX}/${CT_DATA_DIR}"
+cp -R "${STAGE_DIR}/${CT_DATA_DIR}/" "${PREFIX}/${CT_DATA_DIR}"
+CT_DOC_DIR="share/cantera/doc"
+mkdir -p "${PREFIX}/${CT_DOC_DIR}"
+cp -R "${STAGE_DIR}/${CT_DOC_DIR}/" "${PREFIX}/${CT_DOC_DIR}"
+CT_LIB_DIR="lib/cantera/matlab"
+mkdir -p "${PREFIX}/${CT_LIB_DIR}"
+cp -R "${STAGE_DIR}/${CT_LIB_DIR}/" "${PREFIX}/${CT_LIB_DIR}"
+
 set +xe
+
+echo "****************************"
+echo "MATLAB BUILD COMPLETED SUCCESSFULLY"
+echo "****************************"
