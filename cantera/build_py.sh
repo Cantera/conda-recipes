42ecc8d9558c63435259cc2426e769025400ddd7
Bryan W. Weber <bryan.w.weber@gmail.com>
2019-07-03 16:11:11 -0400

    Remove built Python files before building
    The install command was picking up the library for the previous Python
    version and including it in the installed package. This should prevent
    that from happening.

--- cantera/build_py.sh
+++ cantera/build_py.sh
@@ -1,11 +1,21 @@
 echo "****************************"
-echo "PYTHON ${PYTHON} BUILD STARTED"
+echo "PYTHON ${PY_VER} BUILD STARTED"
 echo "****************************"
+
+# Remove old Python build files, if they're present
+if [ -d "build/python" ]; then
+    rm -r build/python
+    rm -r build/temp-py
+    rm interfaces/cython/setup.py
+    rm -r interfaces/cython/build
+    rm -r interfaces/cython/dist
+    rm -r interfaces/cython/Cantera.egg-info
+fi
 
 scons build python_package='y' python_cmd="${PYTHON}"
 
 echo "****************************"
-echo "PYTHON ${PYTHON} BUILD COMPLETED SUCCESSFULLY"
+echo "PYTHON ${PY_VER} BUILD COMPLETED SUCCESSFULLY"
 echo "****************************"
 
 cd interfaces/cython
