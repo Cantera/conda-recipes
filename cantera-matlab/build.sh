a722435f5abe31cb036ad89430074c85248914d5
Bryan Weber <bryan.w.weber@gmail.com>
2022-03-05 19:42:27 -0500

    The Matlab recipe shouldn't use conda compilers

--- cantera-matlab/build.sh
+++ cantera-matlab/build.sh
@@ -13,31 +13,6 @@ echo "boost_inc_dir = '${PREFIX}/include'" >> cantera.conf
 STAGE_DIR="stage"
 echo "stage_dir = '${STAGE_DIR}'" >> cantera.conf
 
-if [[ "${OSX_ARCH}" == "" ]]; then
-    echo "CC = '${CC}'" >> cantera.conf
-    echo "CXX = '${CXX}'" >> cantera.conf
-    echo "cc_flags = '${CFLAGS}'" >> cantera.conf
-    echo "cxx_flags = '${CPPFLAGS}'" >> cantera.conf
-    echo "optimize_flags = ''" >> cantera.conf
-    echo "debug = False" >> cantera.conf
-    echo "use_rpath_linkage = False" >> cantera.conf
-    echo "no_debug_linker_flags = '${LDFLAGS}'" >> cantera.conf
-    echo "renamed_shared_libraries = False" >> cantera.conf
-    echo "VERBOSE = True" >> cantera.conf
-else
-    echo "CC = '${CLANG}'" >> cantera.conf
-    echo "CXX = '${CLANGXX}'" >> cantera.conf
-    echo "cc_flags = '-isysroot ${CONDA_BUILD_SYSROOT} ${CFLAGS}'" >> cantera.conf
-    echo "cxx_flags = '${CPPFLAGS}'" >> cantera.conf
-    echo "optimize_flags = ''" >> cantera.conf
-    echo "debug = False" >> cantera.conf
-    echo "no_debug_linker_flags = '${LDFLAGS} -isysroot ${CONDA_BUILD_SYSROOT}'" >> cantera.conf
-    echo "env_vars = 'LD'" >> cantera.conf
-    echo "VERBOSE = True" >> cantera.conf
-    echo "renamed_shared_libraries = False" >> cantera.conf
-    echo "use_rpath_linkage = False" >> cantera.conf
-fi
-
 echo "matlab_toolbox = 'y'" >> cantera.conf
 echo "matlab_path = '${MW_HEADERS_DIR}'" >> cantera.conf
 
