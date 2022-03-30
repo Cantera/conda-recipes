2ca832605bf24d06d4f76adb2a32450c10a38773
Bryan Weber <bryan.w.weber@gmail.com>
2022-03-30 12:23:56 -0400

    Revert "Try to fix matlab builds"
    This reverts commit 73d14810d60ff9a0a5de6588b6c1a003a7ed6a75.
    The changes weren't needed because the problem was in SConstruct in the
    main Cantera source.

--- cantera-matlab/build.sh
+++ cantera-matlab/build.sh
@@ -6,9 +6,8 @@ echo "****************************"
 
 cp "${RECIPE_DIR}/../.ci_support/cantera_base.conf" cantera.conf
 
-echo "prefix = 'prefix_dir'" >> cantera.conf
+# echo "prefix = ''" >> cantera.conf
 echo "boost_inc_dir = '${PREFIX}/include'" >> cantera.conf
-echo "layout = 'compact'" >> cantera.conf
 
 # Stage the files to make copying easier later
 STAGE_DIR="stage"
@@ -39,16 +38,16 @@ ${BUILD_PREFIX}/bin/python `which scons` install
 # prevent this package from clobbering any existing
 # libcantera or Cantera Python interface files, except the
 # data files and the license file.
-CT_SAMPLES_DIR="prefix_dir/samples/matlab"
-mkdir -p "${PREFIX}/share/cantera/${CT_SAMPLES_DIR}"
-cp -R "${STAGE_DIR}/${CT_SAMPLES_DIR}/" "${PREFIX}/share/cantera/${CT_SAMPLES_DIR}"
-CT_DATA_DIR="prefix_dir/data"
-mkdir -p "${PREFIX}/share/cantera/${CT_DATA_DIR}"
-cp -R "${STAGE_DIR}/${CT_DATA_DIR}/" "${PREFIX}/share/cantera/${CT_DATA_DIR}"
-# CT_DOC_DIR="cantera/doc"
-# mkdir -p "${PREFIX}/${CT_DOC_DIR}"
-# cp -R "${STAGE_DIR}/${CT_DOC_DIR}/" "${PREFIX}/${CT_DOC_DIR}"
-CT_LIB_DIR="prefix_dir/matlab"
+CT_SAMPLES_DIR="share/cantera/samples/matlab"
+mkdir -p "${PREFIX}/${CT_SAMPLES_DIR}"
+cp -R "${STAGE_DIR}/${CT_SAMPLES_DIR}/" "${PREFIX}/${CT_SAMPLES_DIR}"
+CT_DATA_DIR="share/cantera/data"
+mkdir -p "${PREFIX}/${CT_DATA_DIR}"
+cp -R "${STAGE_DIR}/${CT_DATA_DIR}/" "${PREFIX}/${CT_DATA_DIR}"
+CT_DOC_DIR="share/cantera/doc"
+mkdir -p "${PREFIX}/${CT_DOC_DIR}"
+cp -R "${STAGE_DIR}/${CT_DOC_DIR}/" "${PREFIX}/${CT_DOC_DIR}"
+CT_LIB_DIR="lib/cantera/matlab"
 mkdir -p "${PREFIX}/${CT_LIB_DIR}"
 cp -R "${STAGE_DIR}/${CT_LIB_DIR}/" "${PREFIX}/${CT_LIB_DIR}"
 
