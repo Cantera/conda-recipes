c95a2947a2737a114a7431d5814ee460fdf872ed
Ingmar Schoegl <ischoegl@lsu.edu>
2022-08-09 22:06:12 -0500

    Update files removed in build_lib.bat
    Fixes #20

--- cantera/build_lib.bat
+++ cantera/build_lib.bat
@@ -3,8 +3,14 @@ echo "****************************"
 echo "DELETING files from devel except shared libraries and data"
 echo "****************************"
 
-rd /s /q %PREFIX%\Library\share\cantera\doc
-rd /s /q %PREFIX%\Library\share\cantera\samples
-rd /s /q %PREFIX%\Library\share\man
-rd /s /q %PREFIX%\Library\share
+rd /s /q %PREFIX%\share\cantera\doc
+rd /s /q %PREFIX%\share\cantera\samples
+rd /s /q %PREFIX%\share\man
 rd /s /q %PREFIX%\Library\include\cantera
+rd /s /q %PREFIX%\Scripts
+del /f %PREFIX%\Library\lib\cantera.lib
+del /f %PREFIX%\Library\lib\cantera_shared.lib
+
+echo "****************************"
+echo "DELETING files COMPLETED"
+echo "****************************"
--- cantera/build_py.bat
+++ cantera/build_py.bat
@@ -2,20 +2,23 @@ echo ****************************
 echo PYTHON %PYTHON% BUILD STARTED
 echo ****************************
 
-COPY cantera.conf cantera.conf.bak
-DEL /F cantera.conf
-FINDSTR /V "python_package" cantera.conf.bak > cantera.conf
-DEL /F cantera.conf.bak
+:: Remove old Python build files, if they are present
+IF EXIST "build/python" RD /S /Q "build/python"
+IF EXIST "build/temp-py" RD /S /Q "build/temp-py"
+IF EXIST "%PREFIX%/bin/ck2yaml" DEL /F "%PREFIX%/bin/ck2yaml"
+IF EXIST "%PREFIX%/bin/cti2yaml" DEL /F "%PREFIX%/bin/cti2yaml"
+IF EXIST "%PREFIX%/bin/ctml2yaml" DEL /F "%PREFIX%/bin/ctml2yaml"
+IF EXIST "%PREFIX%/bin/yaml2ck" DEL /F "%PREFIX%/bin/yaml2ck"
 
-ECHO python_package='full' >> cantera.conf
 SET "ESC_PYTHON=%PYTHON:\=/%"
 ECHO python_cmd="%ESC_PYTHON%" >> cantera.conf
-CALL scons build
+
+CALL scons build python_package=y
+IF ERRORLEVEL 1 EXIT 1
+
+"%PYTHON%" -m pip install --no-deps --no-index --find-links=build\python\dist\ cantera
 IF ERRORLEVEL 1 EXIT 1
 
 echo ****************************
 echo PYTHON %PYTHON% BUILD COMPLETED SUCCESSFULLY
 echo ****************************
-
-"%PYTHON%" -m pip install --no-deps --no-index --find-links=build\python\dist\ cantera
-IF ERRORLEVEL 1 EXIT 1
