3e16812040e8f7569afb1a759a14c746cd96f4ae
Ingmar Schoegl <ischoegl@lsu.edu>
2022-08-11 07:25:37 -0500

    Package clean cxx sample folders with libcantera-devel

--- cantera/build_devel.bat
+++ cantera/build_devel.bat
@@ -4,6 +4,11 @@ ECHO ************************
 
 CALL scons install
 
+RD /S /Q "%PREFIX%/share/cantera/samples"
+ROBOCOPY "%SRC_DIR%/samples/cxx" "%PREFIX%/share/cantera/samples/cxx" /S /E
+DEL /F "%PREFIX%/share/cantera/samples/cxx/SCons*"
+DEL /F "%PREFIX%/share/cantera/samples/cxx/*Make*"
+
 ECHO ************************
 ECHO DEVEL BUILD COMPLETED SUCCESSFULLY
 ECHO ************************
