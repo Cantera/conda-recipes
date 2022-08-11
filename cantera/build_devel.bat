c47440cb579c65cc691848194c576ae3fc5811fb
Ingmar Schoegl <ischoegl@lsu.edu>
2022-08-11 08:34:35 -0500

    Add fortran f90 to libcantera-devel

--- cantera/build_devel.bat
+++ cantera/build_devel.bat
@@ -4,11 +4,6 @@ ECHO ************************
 
 CALL scons install
 
-RD /S /Q "%PREFIX%/share/cantera/samples"
-ROBOCOPY "%SRC_DIR%/samples/cxx" "%PREFIX%/share/cantera/samples/cxx" /S /E
-DEL /F "%PREFIX%/share/cantera/samples/cxx/SCons*"
-DEL /F "%PREFIX%/share/cantera/samples/cxx/*Make*"
-
 ECHO ************************
 ECHO DEVEL BUILD COMPLETED SUCCESSFULLY
 ECHO ************************
--- cantera/build_devel.sh
+++ cantera/build_devel.sh
@@ -7,11 +7,6 @@ set -e
 ${BUILD_PREFIX}/bin/python `which scons` install
 set +e
 
-rm -r $PREFIX/share/cantera/samples
-cp -r $SRC_DIR/samples/cxx $PREFIX/share/cantera/samples/cxx
-rm $PREFIX/share/cantera/samples/cxx/SCons*
-rm $PREFIX/share/cantera/samples/cxx/*Make*
-
 echo "*****************************"
 echo "DEVEL LIBRARY BUILD COMPLETED"
 echo "*****************************"
