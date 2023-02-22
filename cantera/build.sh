6982710a5e2b903026c49ed3e7412b62b4176a6b
Ray Speth <speth@mit.edu>
2023-02-21 21:26:06 -0500

    Keep default C++ flags (for C++17 support)
    CPPFLAGS is just the preprocessor flags anyway

--- cantera/build.sh
+++ cantera/build.sh
@@ -15,7 +15,6 @@ if [[ "${OSX_ARCH}" == "" ]]; then
     # echo "blas_lapack_libs = 'mkl_rt,dl'" >> cantera.conf
     echo "blas_lapack_dir = '${PREFIX}/lib'" >> cantera.conf
     echo "cc_flags = '${CFLAGS}'" >> cantera.conf
-    echo "cxx_flags = '${CPPFLAGS}'" >> cantera.conf
     echo "optimize_flags = ''" >> cantera.conf
     echo "debug = False" >> cantera.conf
     echo "use_rpath_linkage = False" >> cantera.conf
@@ -28,7 +27,6 @@ else
     echo "blas_lapack_libs = 'openblas'" >> cantera.conf
     echo "blas_lapack_dir = '${PREFIX}/lib'" >> cantera.conf
     echo "cc_flags = '-isysroot ${CONDA_BUILD_SYSROOT} ${CFLAGS}'" >> cantera.conf
-    echo "cxx_flags = '${CPPFLAGS}'" >> cantera.conf
     echo "optimize_flags = ''" >> cantera.conf
     echo "debug = False" >> cantera.conf
     echo "no_debug_linker_flags = '${LDFLAGS} -isysroot ${CONDA_BUILD_SYSROOT}'" >> cantera.conf
