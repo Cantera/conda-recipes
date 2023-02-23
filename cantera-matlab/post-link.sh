3f1b0505e301c295d3a02ca0b3c8875c18033ce2
Ray Speth <speth@mit.edu>
2023-02-22 22:47:55 -0500

    Fix startup.m modification scripts on *nix

--- cantera-matlab/post-link.sh
+++ cantera-matlab/post-link.sh
@@ -9,7 +9,7 @@ MATLAB_STARTUP="${HOME}/Documents/MATLAB/startup.m"
 CONDA_ENV=${PREFIX##*/}
 
 if [ -f "${MATLAB_STARTUP}" ]; then
-    sed -i '.cantera.bak' "/.* % added by Cantera Conda installer for ${CONDA_ENV} environment$/d" "${MATLAB_STARTUP}"
+    sed -i.cantera.bak "/.* % added by Cantera ${PKG_VERSION} Conda installer for ${CONDA_ENV} environment$/d" "${MATLAB_STARTUP}"
 fi
 
 echo "
