9fd7f30d3cf3c5f0ca44a0b178f73d1e69f11820
Ray Speth <speth@mit.edu>
2023-02-22 23:21:29 -0500

    Fix startup.m modifications on Windows

--- cantera-matlab/pre-unlink.bat
+++ cantera-matlab/pre-unlink.bat
@@ -1,9 +1,9 @@
 @ECHO off
-SET "MATLAB_STARTUP=%USERPROFILE%\Documents\MATLAB\startup.m"
+FOR /F "usebackq tokens=* delims=" %%A in (`powershell -ExecutionPolicy Bypass -Command "[Environment]::GetFolderPath('MyDocuments')"`) do @set "MATLAB_STARTUP=%%A\MATLAB\startup.m"
 
 REM Set a variable with the name of the conda environment
-for %%A in ("%PREFIX%\") do set CONDA_ENV=%%~nxA
+for %%A in ("%PREFIX%") do set CONDA_ENV=%%~nxA
 
 REM If the startup.m file already exists, delete any lines added by this script
 IF EXIST "%MATLAB_STARTUP%" COPY "%MATLAB_STARTUP%" "%MATLAB_STARTUP%.cantera.bak"
-IF EXIST "%MATLAB_STARTUP%.cantera.bak" FINDSTR /V "%% added by Cantera Conda Installer for %CONDA_ENV% environment" "%MATLAB_STARTUP%.cantera.bak" > "%MATLAB_STARTUP%"
+IF EXIST "%MATLAB_STARTUP%.cantera.bak" FINDSTR /V /C:"%% added by Cantera %PKG_VERSION% Conda Installer for %CONDA_ENV% environment" "%MATLAB_STARTUP%.cantera.bak" > "%MATLAB_STARTUP%"
