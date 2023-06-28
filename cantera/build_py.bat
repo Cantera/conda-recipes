00e6ca279fec269002d4faf2956ff2af671e1632
Ray Speth <speth@mit.edu>
2023-06-28 15:44:01 -0400

    Debugging persistent cantera_pythonX_Y.dll files

--- cantera/build_py.bat
+++ cantera/build_py.bat
@@ -1,6 +1,14 @@
-echo ****************************
-echo PYTHON %PYTHON% BUILD STARTED
-echo ****************************
+echo on
+
+@echo ****************************
+@echo PYTHON %PYTHON% BUILD STARTED
+@echo ****************************
+
+pwd
+
+dir build
+dir build\lib
+dir %PREFIX%\Library\bin
 
 :: Remove old Python build files, if they are present
 IF EXIST "build/python" RD /S /Q "build/python"
@@ -12,6 +20,10 @@ IF EXIST "%PREFIX%/bin/yaml2ck" DEL /F "%PREFIX%/bin/yaml2ck"
 IF EXIST "%PREFIX%/Lib/site-packages/cantera" DEL /F "%PREFIX%/Lib/site-packages/cantera"
 DEL /F "build/lib/cantera_python*.dll"
 DEL /F "%PREFIX%/Library/bin/cantera_python3_*.dll"
+
+dir build
+dir build\lib
+dir %PREFIX%\Library\bin
 
 SET "ESC_PYTHON=%PYTHON:\=/%"
 ECHO python_cmd="%ESC_PYTHON%" >> cantera.conf
@@ -34,6 +46,6 @@ del /f "%PREFIX%\Lib\site-packages\cantera\cantera_shared.dll"
 
 ROBOCOPY "%SRC_DIR%\samples\python" "%PREFIX%\share\cantera\samples\python" /S /E
 
-echo ****************************
-echo PYTHON %PYTHON% BUILD COMPLETED SUCCESSFULLY
-echo ****************************
+@echo ****************************
+@echo PYTHON %PYTHON% BUILD COMPLETED SUCCESSFULLY
+@echo ****************************
